e4da91b285eeab2f6bd72245325457d98fc9e9d9
willxcience <wilwu@ucdavis.edu>
2016-10-16 18:26:43 -0700

    Add files via upload

---
+++ UDPPingerClient.py
@@ -0,0 +1,20 @@
+import random
+import time
+from socket import *
+
+UDP_Addr = ('128.120.211.98', 24001)
+
+#similar to the server set up
+#set up a socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+for i in range(1,10):
+    message = "Hello Host!!!"
+    clientSocket.sendto(message.encode(), UDP_Addr)
+
+#listener
+while True:
+    reply, addr = clientSocket.recvfrom(1024)
+    print (reply)
+
+clientSocket.close()
